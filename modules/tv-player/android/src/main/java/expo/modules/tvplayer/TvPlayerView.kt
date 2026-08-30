3a7b720b246d1ac30b3b5f674210ab3412866a24
dereferencex <workforpavan@gmail.com>
2026-08-30 14:49:33 +0530

    fix(player): add missing android.view.View import

--- modules/tv-player/android/src/main/java/expo/modules/tvplayer/TvPlayerView.kt
+++ modules/tv-player/android/src/main/java/expo/modules/tvplayer/TvPlayerView.kt
@@ -14,6 +14,7 @@ import android.util.Log
 import android.util.Rational
 import android.view.Gravity
 import android.view.Surface
+import android.view.View
 import android.view.SurfaceView
 import android.view.TextureView
 import android.view.ViewGroup
